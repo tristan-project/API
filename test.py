b12e75b26ebbda9a344ba2b59309f305dc8b118a
tristan-project <r0784105@student.thomasmore.be>
2023-08-12 18:07:58 +0200

    structure

--- test.py
+++ test.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
+from pydantic import BaseModel, EmailStr
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+
+class UserIn(BaseModel):
+    username: str
+    password: str
+    email: EmailStr
+    full_name: str | None = None
+
+
+class UserOut(BaseModel):
+    username: str
+    email: EmailStr
+    full_name: str | None = None
+
+
+@app.post("/user/", response_model=UserOut)
+async def create_user(user: UserIn):
+    return user
+
